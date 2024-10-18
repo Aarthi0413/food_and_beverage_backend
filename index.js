1b4a005814e2d17763b6f717a82ce1417a862302
Aarthi0413 <Aarthi.Murugesan@gwcdata.ai>
2024-10-18 12:47:04 +0530

    added frontend domo url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const router = require("./routes/index");
@@ -10,25 +10,25 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-  {
-    origin: "http://localhost:3000", //https://703917ee-e833-4e24-b6fd-3bd98be9e116.domoapps.prod5.domo.com
+app.use(
+  cors({
+    origin:
+      "https://703917ee-e833-4e24-b6fd-3bd98be9e116.domoapps.prod5.domo.com",
     credentials: true,
-    allowedHeaders:['Content-Type', 'Authorization']
-  }
-));
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Credentials', 'true');
+  res.set("Access-Control-Allow-Credentials", "true");
   next();
 });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Food and Beverage Backend API" });
-})
+});
 
 app.use("/api", router);
-
 
 const PORT = process.env.PORT || 8000;
 
